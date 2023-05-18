43181c40eebf24e624e5a65af96642d71d9a423f
Kneba <abenkenary3@gmail.com>
2023-05-18 15:11:19 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -47,9 +47,7 @@ ClangPath=${MainClangZipPath}
 mkdir $ClangPath
 rm -rf $ClangPath/*
 
-git clone --depth=1 https://gitlab.com/ImSurajxD/clang-r450784d -b master $ClangPath
-#wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/master/clang-r458507.tar.gz -O "clang-r458507.tar.gz"
-#tar -xf clang-r458507.tar.gz -C $ClangPath
+git clone --depth=1 https://github.com/ThankYouMario/proprietary_vendor_qcom_sdclang -b 14 $ClangPath
 
 # Clone GCC
 mkdir $GCCaPath
@@ -61,12 +59,12 @@ tar -xf gcc32.tar.gz -C $GCCbPath
 
 # Prepared
 KERNEL_ROOTDIR=$(pwd)/kernel # IMPORTANT ! Fill with your kernel source root directory.
-export LD=ld.lld
+#export LD=ld.lld
 export KBUILD_BUILD_USER=queen # Change with your own name or else.
 IMAGE=$(pwd)/kernel/out/arch/arm64/boot/Image.gz-dtb
-CLANG_VER="$("$ClangPath"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
-LLD_VER="$("$ClangPath"/bin/ld.lld --version | head -n 1)"
-export KBUILD_COMPILER_STRING="$CLANG_VER with $LLD_VER"
+#CLANG_VER="$("$ClangPath"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+#LLD_VER="$("$ClangPath"/bin/ld.lld --version | head -n 1)"
+#export KBUILD_COMPILER_STRING="$CLANG_VER"
 DATE=$(date +"%F-%S")
 START=$(date +"%s")
 
